f0c86bf246a878d41019544a1fdb50fea5219b0b
Nadishan Chathuranga <nadishanchathuranga21@gmail.com>
2025-07-16 13:34:57 +0530

    project setup

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        if (!\App\Models\User::where('email', 'admin@admin.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 'admin',
+        'email' => 'admin@admin.com',
+        'role_type' => 'Admin',
+        'password' => Hash::make('123456789'),
+    ]);
+}
+
+if (!\App\Models\User::where('email', 'manager@manager.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 'manager',
+        'email' => 'manager@manager.com',
+        'role_type' => 'Manager',
+        'password' => Hash::make('Str0ngM@nagerP@55!'),
+    ]);
+}
+if (!\App\Models\User::where('email', 'sadeepa@roselandtea.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 'sadeepa',
+        'email' => 'sadeepa@roselandtea.com',
+        'role_type' => 'Manager',
+        'password' => Hash::make('Sadeepa@dresshub'),
+    ]);
+}
+
+if (!\App\Models\User::where('email', 'lakshitha@roselandtea.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 'lakshitha',
+        'email' => 'lakshitha@roselandtea.com',
+        'role_type' => 'Manager',
+        'password' => Hash::make('Lakshitha@dresshub'),
+    ]);
+}
+
+if (!\App\Models\User::where('email', 'thisandi@roselandtea.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 'thisandi',
+        'email' => 'thisandi@roselandtea.com',
+        'role_type' => 'Manager',
+        'password' => Hash::make('Thisandi@dresshub'),
+    ]);
+}
+
+if (!\App\Models\User::where('email', 'dharshana@roselandtea.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 'dharshana',
+        'email' => 'dharshana@roselandtea.com',
+        'role_type' => 'Manager',
+        'password' => Hash::make('Dharshana@dresshub'),
+    ]);
+}
+
+if (!\App\Models\User::where('email', 't1@cashier.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 't1',
+        'email' => 't1@cashier.com',
+        'role_type' => 'Cashier',
+        'password' => Hash::make('C@sh1erR@ndom!1'),
+    ]);
+}
+
+if (!\App\Models\User::where('email', 'demo@demo.com')->exists()) {
+    \App\Models\User::factory()->create([
+        'name' => 'demo',
+        'email' => 'demo@demo.com',
+        'role_type' => 'Admin',
+        'password' => Hash::make('D3moStr0ngP@ss!'),
+    ]);
+}
+
+
+
+
+        // $this->call([
+        //     ColorSeeder::class,
+        //     SizeSeeder::class,
+        // ]);
+    }
+}
